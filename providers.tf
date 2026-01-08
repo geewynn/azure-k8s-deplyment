8cec16bd66164ddea50a0fd24eec5f4528a1a855
geewynn <ekainugodwin@gmail.com>
2026-01-08 13:20:33 +0000

    feat: azure k8s deployment

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  subscription_id=var.subscription_id
+  features {}
+}
---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "subscription_id" {
+  description = "subscription id"
+  type        = string
+  sensitive   = false
+}
+
+variable "resource_group_name" {
+  description = "resource_group_name"
+  type = string
+}
+
+variable "resource_group_location" {
+  description = "resource group location"
+  type = string
+}
+
+variable "cluster_name" {
+  description = "k8s cluster name"
+  type = string
+}
+
+variable "dns_prefix" {
+  description = "dns prefix"
+  type = string
+}
+
+
+variable "node_pool_name" {
+  description = "node pool name"
+  type = string
+}
+
+variable "node_count" {
+  description = "node pool count"
+}
+
+variable "vm_size" {
+  description = "vm size"
+  type = string
+}
+
+
