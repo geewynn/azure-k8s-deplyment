3f0e84cee83e3565aacdc0f54706b67acd346944
geewynn <ekainugodwin@gmail.com>
2026-01-17 12:44:30 +0000

    feat: n8n deployment

--- iac/variables.tf
+++ iac/variables.tf
@@ -39,4 +39,24 @@ variable "vm_size" {
   type = string
 }
 
+variable "db_host" {
+  description = "db host"
+  type = string
+}
+
+variable "db_port" {
+  description = "db port"
+  type = string
+}
+
+
+variable "db_user" {
+  description = "db user"
+  type = string
+}
+
+variable "db_password" {
+  description = "db password"
+  type = string
+}
 
